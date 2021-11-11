a2d7ed1ff1a58debd544c3956e7085b6e35216f6
Deepthi Kaithi <89281610+deepthi39@users.noreply.github.com>
2021-11-11 01:02:22 -0600

    Added files

--- app.js
+++ app.js
@@ -3,18 +3,69 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var resourceRouter = require("./routes/resource");  
+const connectionString =
+process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString,
+{useNewUrlParser: true,
+useUnifiedTopology: true});
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded")});
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ballRouter = require('./routes/ball');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
+var resourceRouter = require("./routes/resource");
+var balls = require("./models/ball");
+
+// We can seed the collection if needed on server start
+async function recreateDB() {
+  // Delete everything
+  await balls.deleteMany();
+  let instance1 = new balls({
+    ballType: "Tennis",
+    price: 100,
+    color: "blue",
+  });
+  instance1.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved");
+  });
+  let instance2 = new balls({
+    ballType: "football",
+    price: 200,
+    color: "blue",
+  });
+  instance2.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Second object saved");
+  });
+  let instance3 = new balls({
+    ballType: "basketball",
+    price: 30,
+    color: "blue",
+  });
+  instance3.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Third object saved");
+  });
+}
+let reseed = true;
+if (reseed) {
+  recreateDB();
+}
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+app.use("/resource", resourceRouter);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,6 +77,7 @@ app.use('/users', usersRouter);
 app.use('/balls', ballRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
+app.use("/resource", resourceRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
